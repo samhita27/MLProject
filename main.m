8466bfb985cbd5ac1d57aa11f0899fd83e11a9e6
Samhita S <baandini@gmail.com>
2015-11-15 22:17:27 +0530

    Update main.m

--- main.m
+++ main.m
@@ -1,12 +1,23 @@
 clear all
 clear workspace
 
+VLFEATROOT = '/Users/samhitathakur/USC/Projects/EE660/vlfeat-0.9.20'
+
+s = strcat(VLFEATROOT,'/toolbox/vl_setup');
+
+run(s);
+
 D1 = load ('/Users/samhitathakur/USC/Projects/EE660/cifar-10-batches-mat/data_batch_1.mat')
 [row,col] = size(D1.data)
 
 all_descriptors = [];
 count = 0;
 
+k = 50; %Number of codewords
+
+d = []; % #D array to store descriptors for each image
+
+count_descr = zeros(row,1) ; %Number of descriptors for each image
 
 for i= 1:row
     R=D1.data(i,1:1024);
@@ -16,12 +27,27 @@ for i= 1:row
     A(:,:,2)=reshape(G,32,32);
     A(:,:,3)=reshape(B,32,32);
     I = single(rgb2gray(A));
-    [f,d] = vl_sift(I);
-    if isempty(d)
-        count = count + 1;
-        disp(i);
-    end
-    all_descriptors = [all_descriptors;d'];
+    [frame,descr] = vl_phow(I,'Verbose',2,'Sizes',7,'Step',5,'Color','gray');
+    d(:,:,i) = descr';
+    count_descr(i,1) = size(descr,2);%Number of columns of descr gives the num of feature vectors for the image
+    all_descriptors = [all_descriptors;descr'];
 end
 
-disp(count);
+[dsift_pca_descr,score,latent] = pca(double(all_descriptors'));
+
+[idx,C] = kmeans(double(dsift_pca_descr),k);
+
+bow = zeros(row,k);
+
+t = 1;
+
+for i=1:row
+   for j = t:t+count_descr(i,1)-1
+       bow(i,idx(j))= bow(i,idx(j)) + 1;
+   end
+   t = t + count_descr(i,1);
+end
+
+
+
+
